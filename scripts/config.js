68284e385f5de0841b2083701f62f9cead8b1060
Jerika Kanz <jerikacapila@gmail.com>
2021-12-22 20:49:20 +0100

    Tictactoe: Storing & Managing Stored Data

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = "block";
   backDropElement.style.display = "block";
 }
@@ -8,6 +9,7 @@ function closePlayerConfig() {
   backDropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -21,4 +23,18 @@ function savePlayerConfig(event) {
     errorsOutputElement.textContent = "Please enter a valid name!";
     return;
   }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  // if (editedPlayer === 1) {
+  //   players[0].name = enteredPlayername;
+  // } else {
+  //   players[1].name = enteredPlayername;
+  // }
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
