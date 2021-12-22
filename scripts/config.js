442be13997eea20cecb43ac75dd6f4dec45ba56b
Jerika Kanz <jerikacapila@gmail.com>
2021-12-22 19:35:19 +0100

    Tictactoe: Validating User Input With JavaScript

--- scripts/config.js
+++ scripts/config.js
@@ -6,11 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backDropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername");
-  console.log(enteredPlayername);
+  const enteredPlayername = formData.get("playername").trim(); // .trim() method gets rid of all white spaces before and/or after the content
+
+  if (!enteredPlayername) {
+    // enteredPlayername === ""; if enteredPlayername is falsy/empty:
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
