426802639fa2878595a839dd4f84d48315eccb9f
Jerika Kanz <jerikacapila@gmail.com>
2021-12-22 15:29:19 +0100

    Tictactoe: Showing & Hiding The Configuration Modal

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,9 @@
-function openPlayerConfig() {}
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backDropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backDropElement.style.display = "none";
+}
