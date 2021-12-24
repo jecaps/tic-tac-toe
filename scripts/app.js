aa4f8e988025707939c8533f05ed4c2b766a8db5
Jerika Kanz <jerikacapila@gmail.com>
2021-12-24 20:37:16 +0100

    Tictactoe: Managing Game Rounds

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 let editedPlayer = 0;
-
+let activePlayer = 0;
 const players = [
   {
     name: "",
@@ -16,11 +16,14 @@ const backDropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li");
+const gameBoardElement = document.getElementById("game-board");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -31,3 +34,9 @@ backDropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener("click", selectGameField);
+// }
+
+gameBoardElement.addEventListener("click", selectGameField);
