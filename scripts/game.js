aabf57f66e65f7729bc47e09cf2cb66aaf01778a
Jerika Kanz <jerikacapila@gmail.com>
2021-12-26 15:01:16 +0100

    Tictactoe: Ending the Game and Adding Restart Logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,31 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innertHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItem = gameBoardElement.children[gameBoardIndex];
+      gameBoardItem.textContent = "";
+      gameBoardItem.classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("Please set custom player names for both players!");
     return;
   }
+
+  resetGameStatus();
+
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
 }
@@ -17,7 +40,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName != "LI") {
+  if (event.target.tagName != "LI" || gameIsOver) {
     return;
   }
 
@@ -35,8 +58,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
 
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
   currentRound++;
   switchPlayer();
 }
@@ -87,4 +112,18 @@ function checkForGameOver() {
   }
 
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      "It's a draw!";
+  }
 }
